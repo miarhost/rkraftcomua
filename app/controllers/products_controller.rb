da0898a3692697838d976f3c778d260405956498
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-21 16:03:00 +0300

    cart changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,9 +50,9 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
 
-       # @products = Product.all
-       # ActionCable.server.broadcast 'products',
-       # html: render_to_string('products/index')
+       @products = Product.all
+       ActionCable.server.broadcast 'products',
+       html: render_to_string('products/index', layout: false)
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
