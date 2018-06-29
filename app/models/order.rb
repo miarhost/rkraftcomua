99f2a601e411fa13d5cf89e7e0277ce22b9fd50b
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-29 14:33:23 +0300

    order validations + errors I18n

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,20 @@
 class Order < ApplicationRecord
  
  has_many :line_items, dependent: :destroy
+ before_save { email.downcase! }
+ REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 	enum pay_type: {
 		"Credit card" => 0,
 		"Purchase order" => 1
 	}
- validates :name, :address, :email, presence: true 
+
+ validates :name, presence: true, length: { minimum: 3 }
+ validates :address, presence: true, length: { minimum: 2, maximum: 150 }
+ validates :email, presence: true, length: { minimum: 12, maximum: 255 },
+                   format: { with: REGEXP },
+                   uniqueness: true
+                   
  validates :pay_type, inclusion: pay_types.keys 
 
     def add_line_items_from_cart(cart)
@@ -15,7 +23,5 @@ class Order < ApplicationRecord
     		line_items << item
     	end
     end
-
-
 
 end
