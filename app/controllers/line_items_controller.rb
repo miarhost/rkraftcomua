107e3bf6bddd7a3627fffd304206a0a5172ba09d
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-30 21:29:37 +0300

    i18n: clamshells page, cart changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,9 +59,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    product = Product.find(params[:product_id])
+    @line_item = @cart.remove_product(product)
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to new_order_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
