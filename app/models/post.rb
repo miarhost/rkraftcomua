f51c137663b0d7336df3109363075cbae585ee8f
MariaTikhonova <ritz.rkraft@gmail.com>
2018-07-10 14:37:08 +0300

    blog posts views, image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+validates :title, presence: true, length: { minimum: 2, maximum: 150 }
+validates :body, presence: true, length: { minimum: 2 }
+
+mount_uploader :image, ImageUploader
+
 end
