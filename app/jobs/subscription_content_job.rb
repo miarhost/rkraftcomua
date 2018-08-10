76671ef0e43cbfd9e8ac8b55cc7159a18bf40771
MariaTikhonova <ritz.rkraft@gmail.com>
2018-08-10 17:55:58 +0300

    fix subscription content job

--- app/jobs/subscription_content_job.rb
+++ app/jobs/subscription_content_job.rb
@@ -2,20 +2,19 @@ class SubscriptionContentJob < ApplicationJob
 self.queue_adapter = :sidekiq
   queue_as :low_priority
  sidekiq_options retry: 5
- around_perform :around_delivering
 
-  def perform(subscription_mailer)
-  	subscription_mailer = ActionMailer::SubscriptionMailer.notify(@post)
-    
+
+  def perform(image)
+  	ActionMailer::SubscriptionMailer.notify(@post)
+    if Rails.env.production? 
+     image = @post.image 
+     image.enqueue wait: 24.minutes 
+     next
   	rescue ActiveRecord::RecordNotFound
-     
+    end
   end 
 
-  private
 
-  def around_delivering(job)
-   job.enqueue  queue: ENV["S3_BUCKET"], wait: 5.minutes  
-  end
 
 end
 
