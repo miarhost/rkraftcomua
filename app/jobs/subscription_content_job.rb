e5bc905045a103b86bef0475a22bb5682973155e
MariaTikhonova <ritz.rkraft@gmail.com>
2018-08-10 17:22:14 +0300

    fix subscription content job

--- app/jobs/subscription_content_job.rb
+++ app/jobs/subscription_content_job.rb
@@ -1,12 +1,12 @@
 class SubscriptionContentJob < ApplicationJob
 self.queue_adapter = :sidekiq
   queue_as :low_priority
- 
+ sidekiq_options retry: 5
  around_perform :around_delivering
 
   def perform(subscription_mailer)
   	subscription_mailer = ActionMailer::SubscriptionMailer.notify(@post)
-  	
+    
   	rescue ActiveRecord::RecordNotFound
      
   end 
@@ -14,7 +14,7 @@ self.queue_adapter = :sidekiq
   private
 
   def around_delivering(job)
-   retry 5
+   attempt_retry { :trace => ENV["S3_BUCKET" } 
   end
 
 end
